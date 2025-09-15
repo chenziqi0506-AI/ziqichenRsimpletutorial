65ddfb94af5d6dd31f583c6ae0d0322c14fb5a96
chenziqi0506-AI <chenziqi0506@gmail.com>
2025-09-15 13:57:55 +0800

    Add files via upload

---
+++ lesson1/lesson1.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+sample_data1 <- read.csv(file = "C:/Users/Lenovo/Downloads/R simple tutorial/billboard/hot100.csv")
+tibble_data1 <- as_tibble(sample_data)
+tibble_data1
+sample_data2 <- read.csv(file = "C:/Users/Lenovo/Downloads/R simple tutorial/diamonds.csv")
+tibble_data2 <- as_tibble(sample_data2)
+tibble_data2
